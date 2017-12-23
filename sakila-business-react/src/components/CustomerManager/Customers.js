52b298ad5b12666b31f4b0d3a2512583eeacc182
nicosim <nico93simo@hotmail.fr>
2017-12-23 18:20:34 +0100

    Customer Management

--- sakila-business-react/src/components/CustomerManager/Customers.js
+++ sakila-business-react/src/components/CustomerManager/Customers.js
@@ -10,7 +10,57 @@ import {
     TableHeader
 } from 'material-ui/Table';
 
+import TextField from 'material-ui/TextField/TextField';
+import '../../material-ui.css'
 
+const CustomerForm = (props) => (
+    <form>
+        <span>Name : </span>
+            <TextField
+                name="firstName"
+                type="text"
+                hintText="Enter your first name"
+                className="textfield-class"
+            /> 
+            <TextField
+                name="lasttName"
+                type="text"
+                hintText="Enter your last name"
+                className="textfield-class"
+            />
+        
+        <br/>
+        <span>Email : </span>
+            <TextField
+                name="email"
+                type="text"
+                hintText="Enter your email"
+                className="textfield-class"
+            />
+        
+        <br/>
+        <span>Address : </span>
+            <TextField
+                name="address"
+                type="text"
+                hintText="Enter your address"
+                className="textfield-class"
+            /> 
+            <TextField
+                name="city"
+                type="text"
+                hintText="Enter your city"
+                className="textfield-class"
+            /> 
+            <TextField
+                name="country"
+                type="text"
+                hintText="Enter your country"
+                className="textfield-class"
+            />
+        
+    </form>
+)
 const displayCustomerData = (props) => props.customers.map(c => <CustomerData key={c.customerId} {...c}/>);
 
 const Customers = (props) => 
@@ -29,4 +79,4 @@ Customers.propTypes = {
     customers: PropTypes.array.isRequired
 }
 
-export default Customers;
+export  {Customers, CustomerForm};
