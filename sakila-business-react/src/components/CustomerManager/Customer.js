7db725c6ae94b7f409c8115b8e63adf219473384
Zawartek <masterdu92@hotmail.fr>
2017-12-23 20:02:06 +0100

    Un tableau joli

--- sakila-business-react/src/components/CustomerManager/Customer.js
+++ sakila-business-react/src/components/CustomerManager/Customer.js
@@ -11,41 +11,66 @@ import {
 import RaisedButton from 'material-ui/RaisedButton';
 
 const style = {
-  margin: 2,
+    margin: 2,
 };
+
+const col1 = {
+    width: '5%',
+}
+
+const col2 = {
+    width: '20%',
+}
+
+const col3 = {
+    width: '15%',
+}
+
+const col4 = {
+    width: '20%',
+}
+
+const col5 = {
+    width: '22.5%',
+}
+
+const col6 = {
+    width: '17.5%',
+    textAlign: 'center'
+}
 
 const CustomerHeader = (props) => (
     <TableRow>
-        <TableHeaderColumn>ID</TableHeaderColumn>
-        <TableHeaderColumn>Store Address</TableHeaderColumn>
-        <TableHeaderColumn>Name</TableHeaderColumn>
-        <TableHeaderColumn>Email</TableHeaderColumn>
-        <TableHeaderColumn>Address</TableHeaderColumn>
-        <TableHeaderColumn></TableHeaderColumn>
+        <TableHeaderColumn style={col1}>ID</TableHeaderColumn>
+        <TableHeaderColumn style={col2}>Store address</TableHeaderColumn>
+        <TableHeaderColumn style={col3}>Name</TableHeaderColumn>
+        <TableHeaderColumn style={col4}>Email address</TableHeaderColumn>
+        <TableHeaderColumn style={col5}>Address</TableHeaderColumn>
+        <TableHeaderColumn style={col6}></TableHeaderColumn>
     </TableRow>
 )
 
 const CustomerData = (props) => (
     <TableRow>
-        <TableRowColumn>{props.customerId}</TableRowColumn>
-        <TableRowColumn>
+        <TableRowColumn style={col1}>{props.customerId}</TableRowColumn>
+        <TableRowColumn style={col2}>
             <span>{props.store.address.address} - {props.store.address.city.city} {props.store.address.city.country.country}</span>
-            </TableRowColumn>
-        <TableRowColumn><span>{props.firstName} {props.lastName}</span></TableRowColumn>
-        <TableRowColumn>{props.email}</TableRowColumn>
-        <TableRowColumn>
+        </TableRowColumn>
+        <TableRowColumn style={col3}><span>{props.firstName} {props.lastName}</span></TableRowColumn>
+        <TableRowColumn style={col4}>{props.email}</TableRowColumn>
+        <TableRowColumn style={col5}>
             <span>{props.address.address} - {props.address.city.city} {props.address.city.country.country}</span>
         </TableRowColumn>
-        <TableRowColumn>
-            <RaisedButton label="Edit" labelColor="#FFFFFF" backgroundColor="#4CAF50" style={style} />
+        <TableRowColumn style={col6}>
+            <RaisedButton label="Edit" labelColor="#FFFFFF" backgroundColor="#3F51B5" style={style} />
             <RaisedButton label="Remove" labelColor="#FFFFFF" backgroundColor="#F44336" style={style} />
         </TableRowColumn>
     </TableRow>
-    
+
 )
 
 CustomerData.propTypes = {
     customerId: PropTypes.number.isRequired
 }
 
-export {CustomerHeader, CustomerData};
+export { CustomerHeader, CustomerData };
--- sakila-business-react/src/components/CustomerManager/Customers.js
+++ sakila-business-react/src/components/CustomerManager/Customers.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {CustomerHeader, CustomerData} from './Customer';
+import { CustomerHeader, CustomerData } from './Customer';
 import PropTypes from 'prop-types';
 
 
@@ -15,62 +15,62 @@ import TextField from 'material-ui/TextField/TextField';
 const CustomerForm = (props) => (
     <form>
         <span>Name : </span>
-            <TextField
-                name="firstName"
-                type="text"
-                hintText="Enter your first name"
-                className="textfield-class"
-            /> 
-            <TextField
-                name="lasttName"
-                type="text"
-                hintText="Enter your last name"
-                className="textfield-class"
-            />
-        
-        <br/>
+        <TextField
+            name="firstName"
+            type="text"
+            hintText="Enter your first name"
+            className="textfield-class"
+        />
+        <TextField
+            name="lasttName"
+            type="text"
+            hintText="Enter your last name"
+            className="textfield-class"
+        />
+
+        <br />
         <span>Email : </span>
-            <TextField
-                name="email"
-                type="text"
-                hintText="Enter your email"
-                className="textfield-class"
-            />
-        
-        <br/>
+        <TextField
+            name="email"
+            type="text"
+            hintText="Enter your email"
+            className="textfield-class"
+        />
+
+        <br />
         <span>Address : </span>
-            <TextField
-                name="address"
-                type="text"
-                hintText="Enter your address"
-                className="textfield-class"
-            /> 
-            <TextField
-                name="city"
-                type="text"
-                hintText="Enter your city"
-                className="textfield-class"
-            /> 
-            <TextField
-                name="country"
-                type="text"
-                hintText="Enter your country"
-                className="textfield-class"
-            />
-        
+        <TextField
+            name="address"
+            type="text"
+            hintText="Enter your address"
+            className="textfield-class"
+        />
+        <TextField
+            name="city"
+            type="text"
+            hintText="Enter your city"
+            className="textfield-class"
+        />
+        <TextField
+            name="country"
+            type="text"
+            hintText="Enter your country"
+            className="textfield-class"
+        />
+
     </form>
 )
-const displayCustomerData = (props) => props.customers.map(c => <CustomerData key={c.customerId} {...c}/>);
+const displayCustomerData = (props) => props.customers.map(c => <CustomerData key={c.customerId} {...c} />);
 
-const Customers = (props) => 
+const Customers = (props) =>
     <Table>
-        <TableHeader 
+        <TableHeader
             displaySelectAll={false}
             adjustForCheckbox={false}>
-            <CustomerHeader/>
+            <CustomerHeader />
         </TableHeader>
         <TableBody displayRowCheckbox={false} data={props}>
-            {displayCustomerData({...props})}
+            {displayCustomerData({ ...props })}
         </TableBody>
     </Table>
 
@@ -78,4 +78,4 @@ Customers.propTypes = {
     customers: PropTypes.array.isRequired
 }
 
-export  {Customers, CustomerForm};
+export { Customers, CustomerForm };
