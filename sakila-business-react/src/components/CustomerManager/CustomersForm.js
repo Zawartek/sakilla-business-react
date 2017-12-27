e6c7a7027c5b063786cd010d6ac54e2e59ef27d1
Zawartek <masterdu92@hotmail.fr>
2017-12-27 16:17:39 +0100

    Maj css + default customer state

--- sakila-business-react/src/components/CustomerManager/CustomersForm.js
+++ sakila-business-react/src/components/CustomerManager/CustomersForm.js
@@ -6,90 +6,60 @@ import RaisedButton from 'material-ui/RaisedButton';
 import * as customerService from '../../services/customerService';
 
 const style = {
-    margin: 50,
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: 50,
+    marginRight: 50,
+};
+
+const baseState = {
+    adding: true,
+    formData: {
+        customerId: -1,
+        addressId: -1,
+        firstName: '',
+        lastName: '',
+        email: '',
+        address: '',
+        city: '',
+        country: ''
+    }
 };
 
 class CustomersForm extends Component {
-    
+
     constructor() {
         super();
 
-        this.state = {
-            adding:true,
-            formData: {
-                customerId:-1,
-                addressId:-1,
-                firstName: '',
-                lastName: '',
-                email: '',
-                address: '',
-                city: '',
-                country: ''
-            }
-        };
+        this.state = baseState;
     }
 
-    handleOnClickOnAdd = (e) =>{
-        customerService.createCustomer(this.state.formData).then(res=>{
-        this.setState({
-            adding:true,
-            formData: {
-                customerId:-1,
-                addressId:-1,
-                firstName: '',
-                lastName: '',
-                email: '',
-                address: '',
-                city: '',
-                country: ''
-            }
-        });
-        this.props.reloadCustomers();
+    handleOnClickOnAdd = (e) => {
+        customerService.createCustomer(this.state.formData).then(res => {
+            this.setState(baseState);
+            this.props.reloadCustomers();
         })
     }
 
-    handleOnClickOnUpdate = (e) =>{
-        customerService.updateCustomer(this.state.formData).then(res=>{
-        this.setState({
-            adding:true,
-            formData: {
-                customerId:-1,
-                addressId:-1,
-                firstName: '',
-                lastName: '',
-                email: '',
-                address: '',
-                city: '',
-                country: ''
-            }
-        });
-        this.props.reloadCustomers();
+    handleOnClickOnUpdate = (e) => {
+        customerService.updateCustomer(this.state.formData).then(res => {
+            this.setState(baseState);
+            this.props.reloadCustomers();
         })
     }
 
     handleOnClickOnReset = (e) => {
-        this.setState({
-            adding:true,
-            formData: {
-                customerId:-1,
-                addressId:-1,
-                firstName: '',
-                lastName: '',
-                email: '',
-                address: '',
-                city: '',
-                country: ''
-            }
-        });
+        this.setState(baseState);
     }
 
     handleOnClickOnEdit = (id) => {
+        window.scrollTo(0, 0);
         customerService.getCustomer(id).then(res => {
             this.setState({
-                adding:false,
+                adding: false,
                 formData: {
-                    customerId:res.data.customerId,
-                    addressId:res.data.address.addressId,
+                    customerId: res.data.customerId,
+                    addressId: res.data.address.addressId,
                     firstName: res.data.firstName,
                     lastName: res.data.lastName,
                     email: res.data.email,
@@ -97,120 +67,120 @@ class CustomersForm extends Component {
                     city: res.data.address.city.city,
                     country: res.data.address.city.country.country
                 }
-        });
+            });
 
         })
     }
 
     handleOnChangeOnFirstName = (e) => {
-        this.setState({formData:{...this.state.formData,firstName:e.target.value}});
+        this.setState({ formData: { ...this.state.formData, firstName: e.target.value } });
     }
     handleOnChangeOnLastName = (e) => {
-        this.setState({formData:{...this.state.formData,lastName:e.target.value}});
+        this.setState({ formData: { ...this.state.formData, lastName: e.target.value } });
     }
     handleOnChangeOnEmail = (e) => {
-        this.setState({formData:{...this.state.formData,email:e.target.value}});
+        this.setState({ formData: { ...this.state.formData, email: e.target.value } });
     }
     handleOnChangeOnAddress = (e) => {
-        this.setState({formData:{...this.state.formData,address:e.target.value}});
+        this.setState({ formData: { ...this.state.formData, address: e.target.value } });
     }
     handleOnChangeOnCity = (e) => {
-        this.setState({formData:{...this.state.formData,city:e.target.value}});
+        this.setState({ formData: { ...this.state.formData, city: e.target.value } });
     }
     handleOnChangeOnCountry = (e) => {
-        this.setState({formData:{...this.state.formData,country:e.target.value}});
+        this.setState({ formData: { ...this.state.formData, country: e.target.value } });
     }
 
     render() {
-        const {adding} = this.state
+        const { adding } = this.state
         return (
-        <form>
-            <input type="hidden" value={this.state.formData.customerId}/>
-            <input type="hidden" value={this.state.formData.addressId}/>
-            <span>Name : </span>
-            <TextField
-                name="firstName"
-                type="text"
-                value= {this.state.formData.firstName}
-                onChange= {this.handleOnChangeOnFirstName}
-                hintText='Enter your first name'
-                className="textfield-class"
-            />
-            <TextField
-                name="lastName"
-                type="text"
-                value= {this.state.formData.lastName}
-                onChange= {this.handleOnChangeOnLastName}
-                hintText='Enter your last name'
-                className="textfield-class"
-            />
+            <form>
+                <input type="hidden" value={this.state.formData.customerId} />
+                <input type="hidden" value={this.state.formData.addressId} />
+                <span>Name : </span>
+                <TextField
+                    name="firstName"
+                    type="text"
+                    value={this.state.formData.firstName}
+                    onChange={this.handleOnChangeOnFirstName}
+                    hintText='Enter your first name'
+                    className="textfield-class"
+                />
+                <TextField
+                    name="lastName"
+                    type="text"
+                    value={this.state.formData.lastName}
+                    onChange={this.handleOnChangeOnLastName}
+                    hintText='Enter your last name'
+                    className="textfield-class"
+                />
 
-            <br />
-            <span>Email : </span>
-            <TextField
-                name="email"
-                type="text"
-                value= {this.state.formData.email}
-                onChange= {this.handleOnChangeOnEmail}
-                hintText='Enter your email'
-                className="textfield-class"
-            />
+                <br />
+                <span>Email : </span>
+                <TextField
+                    name="email"
+                    type="text"
+                    value={this.state.formData.email}
+                    onChange={this.handleOnChangeOnEmail}
+                    hintText='Enter your email'
+                    className="textfield-class"
+                />
 
-            <br />
-            <span>Address : </span>
-            <TextField
-                name="address"
-                type="text"
-                value= {this.state.formData.address}
-                onChange= {this.handleOnChangeOnAddress}
-                hintText='Enter your address'
-                className="textfield-class"
-            />
-            <TextField
-                name="city"
-                type="text"
-                value= {this.state.formData.city}
-                onChange= {this.handleOnChangeOnCity}
-                hintText='Enter your city'
-                className="textfield-class"
-            />
-            <TextField
-                name="country"
-                type="text"
-                value= {this.state.formData.country}
-                onChange= {this.handleOnChangeOnCountry}
-                hintText='Enter your country'
-                className="textfield-class"
-            />
-            <div>
-                { adding?
-                <RaisedButton 
-                    label='Add' 
-                    labelColor="#FFFFFF" 
-                    backgroundColor="#3F51B5" 
-                    style={style} 
-                    onClick = {this.handleOnClickOnAdd}
+                <br />
+                <span>Address : </span>
+                <TextField
+                    name="address"
+                    type="text"
+                    value={this.state.formData.address}
+                    onChange={this.handleOnChangeOnAddress}
+                    hintText='Enter your address'
+                    className="textfield-class"
+                />
+                <TextField
+                    name="city"
+                    type="text"
+                    value={this.state.formData.city}
+                    onChange={this.handleOnChangeOnCity}
+                    hintText='Enter your city'
+                    className="textfield-class"
+                />
+                <TextField
+                    name="country"
+                    type="text"
+                    value={this.state.formData.country}
+                    onChange={this.handleOnChangeOnCountry}
+                    hintText='Enter your country'
+                    className="textfield-class"
+                />
+                <div>
+                    {adding ?
+                        <RaisedButton
+                            label='Add'
+                            labelColor="#FFFFFF"
+                            backgroundColor="#3F51B5"
+                            style={style}
+                            onClick={this.handleOnClickOnAdd}
+                        />
+                        :
+                        <RaisedButton
+                            label='Update'
+                            labelColor="#FFFFFF"
+                            backgroundColor="#3F51B5"
+                            style={style}
+                            onClick={this.handleOnClickOnUpdate}
+                        />
+                    }
+                    <RaisedButton
+                        label='Reset'
+                        labelColor="#FFFFFF"
+                        backgroundColor="#F44336"
+                        style={style}
+                        onClick={this.handleOnClickOnReset}
                     />
-                :
-                <RaisedButton 
-                    label='Update' 
-                    labelColor="#FFFFFF" 
-                    backgroundColor="#3F51B5" 
-                    style={style} 
-                    onClick = {this.handleOnClickOnUpdate}
-                    />
-                }
-                <RaisedButton 
-                    label='Reset'
-                    labelColor="#FFFFFF" 
-                    backgroundColor="#F44336" 
-                    style={style} 
-                    onClick = {this.handleOnClickOnReset}
-                    />
-            </div>
-        </form>
+                </div>
+            </form>
         )
     }
 }
 
-export {CustomersForm}
+export { CustomersForm }
--- sakila-business-react/src/helpers/customerDataHelper.js
+++ sakila-business-react/src/helpers/customerDataHelper.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import RaisedButton from "material-ui/RaisedButton";
 import {
-  TableRow,
-  TableRowColumn,
+    TableRow,
+    TableRowColumn,
 } from 'material-ui/Table';
 
 import SelectField from "material-ui/SelectField";
@@ -40,26 +40,26 @@ const col6 = {
 
 const customerDataHelper = {
 
-  displayCustomerRow: (props) => (
-    props.customers.map( (customer) => (
+    displayCustomerRow: (props) => (
+        props.customers.map((customer) => (
 
-        <TableRow key={customer.customerId}>
-        <TableRowColumn style={col1}>{customer.customerId}</TableRowColumn>
-        <TableRowColumn style={col2}>
-            <span>{customer.store.address.address} - {customer.store.address.city.city} {customer.store.address.city.country.country}</span>
-        </TableRowColumn>
-        <TableRowColumn style={col3}><span>{customer.firstName} {customer.lastName}</span></TableRowColumn>
-        <TableRowColumn style={col4}>{customer.email}</TableRowColumn>
-        <TableRowColumn style={col5}>
-            <span>{customer.address.address} - {customer.address.city.city} {customer.address.city.country.country}</span>
-        </TableRowColumn>
-        <TableRowColumn style={col6}>
-            <RaisedButton id={customer.customerId} label="Edit" labelColor="#FFFFFF" backgroundColor="#3F51B5" style={style} onClick={() => props.handleEdit(customer.customerId)}/>
-            <RaisedButton label="Remove" labelColor="#FFFFFF" backgroundColor="#F44336" style={style} onClick={() => props.handleDelete(customer.customerId)} />
-        </TableRowColumn>
-    </TableRow>
-    ))
-  )
+            <TableRow key={customer.customerId}>
+                <TableRowColumn style={col1}>{customer.customerId}</TableRowColumn>
+                <TableRowColumn style={col2}>
+                    <span>{customer.store.address.address} - {customer.store.address.city.city} {customer.store.address.city.country.country}</span>
+                </TableRowColumn>
+                <TableRowColumn style={col3}><span>{customer.firstName} {customer.lastName}</span></TableRowColumn>
+                <TableRowColumn style={col4}><span>{customer.email}</span></TableRowColumn>
+                <TableRowColumn style={col5}>
+                    <span>{customer.address.address} - {customer.address.city.city} {customer.address.city.country.country}</span>
+                </TableRowColumn>
+                <TableRowColumn style={col6}>
+                    <RaisedButton id={customer.customerId} label="Edit" labelColor="#FFFFFF" backgroundColor="#3F51B5" style={style} onClick={() => props.handleEdit(customer.customerId)} />
+                    <RaisedButton label="Remove" labelColor="#FFFFFF" backgroundColor="#F44336" style={style} onClick={() => props.handleDelete(customer.customerId)} />
+                </TableRowColumn>
+            </TableRow>
+        ))
+    )
 }
 
 export default customerDataHelper;
